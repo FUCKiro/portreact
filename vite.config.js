d490bd36b60304af586f3526030cac491acc39c4
FUCKiro <fabio.rock84@gmail.com>
2025-06-05 17:14:00 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-@@ -1,5 +1,14 @@
- import { defineConfig } from 'vite'
- import react from '@vitejs/plugin-react'
-+
-+// Custom middleware to set correct MIME type for GLB files
-+const gltfMiddleware = () => ({
-+  name: 'configure-server',
-+  configureServer(server) {
-+    server.middlewares.use((req, res, next) => {
-+      if (req.url.endsWith('.glb')) res.setHeader('Content-Type', 'model/gltf-binary');
-+      next();
-+    });
-+  }
-+});
- 
- // https://vitejs.dev/config/
- export default defineConfig({
--  plugins: [react()],
-+  plugins: [react(), gltfMiddleware()],
-   assetsInclude: ['**/*.glb'],
-   build: {
-     outDir: 'dist',
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Custom middleware to set correct MIME type for GLB files
+const gltfMiddleware = () => ({
+  name: 'configure-server',
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url.endsWith('.glb')) res.setHeader('Content-Type', 'model/gltf-binary');
+      next();
+    });
+  }
+});
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), gltfMiddleware()],
+  assetsInclude: ['**/*.glb'],
+  build: {
+    outDir: 'dist',
+  }
+})
