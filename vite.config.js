fa3edcabba8a5e36d0e7718f8e2fa1caf9516da3
FUCKiro <fabio.rock84@gmail.com>
2025-06-05 17:14:52 +0200

    Fix Vite configuration for GLB handling

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Custom middleware to set correct MIME type for GLB files
-const gltfMiddleware = () => ({
-  name: 'configure-server',
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
-      if (req.url.endsWith('.glb')) res.setHeader('Content-Type', 'model/gltf-binary');
-      next();
-    });
-  }
-});
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), gltfMiddleware()],
+  plugins: [react()],
   assetsInclude: ['**/*.glb'],
   build: {
     outDir: 'dist',
